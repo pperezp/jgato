d9dbbfbe2adacca1b070a4a4b87835f4382d9200
patodeath@gmail.com <patodeath@gmail.com@1220f760-cf00-aeef-3dda-578c7020cea4>
2012-03-02 15:50:50 +0000

    Primera vez que subo el proyecto netbeans

---
+++ trunk/src/Juego.java
@@ -0,0 +1,117 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Administrador
+ */
+public class Juego {
+
+    private static BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+    private static Jugador jugador1 = new Jugador();
+    private static Jugador jugador2 = new Jugador();
+    private static String figura, respuesta = null;
+    private static Tablero tab;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        do {
+            tab = new Tablero();
+            pedirInformacion();
+            do {
+                juegaJugador(jugador1);
+                juegaJugador(jugador2);
+            } while (!tab.tableroLleno() && !tab.hayGanador());
+            verGanador();
+            try {
+                do {
+                    System.out.println("Otra vez? [si/no]: ");
+                    respuesta = leer.readLine();
+                    if (!respuesta.equalsIgnoreCase("no") && !respuesta.equalsIgnoreCase("si")) {
+                        limpiarPantalla();
+                    }
+                } while (!respuesta.equalsIgnoreCase("no") && !respuesta.equalsIgnoreCase("si"));
+            } catch (IOException e1) {
+            }
+        } while (respuesta.equalsIgnoreCase("si"));
+        limpiarPantalla();
+        System.out.println("Adios");
+    }
+
+    private static void pedirInformacion() {
+        try {
+            limpiarPantalla();
+            System.out.println("Nombre Jugador 1: ");
+            jugador1.setNombre(leer.readLine());
+            do {
+                limpiarPantalla();
+                System.out.println(jugador1.getNombre() + ", elija figura [x/o]: ");
+                figura = leer.readLine();
+            } while (!figura.equalsIgnoreCase("x") && !figura.equalsIgnoreCase("o"));
+            jugador1.setFigura(figura);
+
+            if (jugador1.getFigura().equalsIgnoreCase("x")) {
+                jugador2.setFigura("o");
+            } else {
+                jugador2.setFigura("x");
+            }
+
+
+            limpiarPantalla();
+            System.out.println("Nombre Jugador 2: ");
+            jugador2.setNombre(leer.readLine());
+        } catch (IOException e) {
+        }
+    }
+
+    private static void juegaJugador(Jugador jugador) {
+        try {
+            limpiarPantalla();
+            tab.imprimirTablero();
+            if (!tab.tableroLleno() && !tab.hayGanador()) {
+                do {
+                    do {
+                        System.out.print("Juega " + jugador.getNombre() + "[" + jugador.getFigura() + "] >>>");
+                        jugador.setPosicion(Integer.parseInt(leer.readLine()));
+                    } while (jugador.getPosicion() < 1 || jugador.getPosicion() > 9);
+
+                    if (tab.posicionOcupada(jugador.getPosicion())) {
+                        limpiarPantalla();
+                        tab.imprimirTablero();
+                        System.out.println("Casilla Ocupada!!!");
+                    }
+                } while (tab.posicionOcupada(jugador.getPosicion()));
+                tab.setFigura(jugador.getFigura(), jugador.getPosicion());
+            }
+        } catch (NumberFormatException e) {
+            juegaJugador(jugador);
+        } catch (IOException e) {
+        }
+    }
+
+    private static void verGanador() {
+        if (jugador1.getFigura().equalsIgnoreCase(tab.getGanador())) {
+            System.out.println(jugador1.getNombre() + "[" + jugador1.getFigura() + "]" + " has ganado!!!!!!");
+        } else if (jugador2.getFigura().equalsIgnoreCase(tab.getGanador())) {
+            tab.imprimirTablero();
+            System.out.println(jugador2.getNombre() + "[" + jugador2.getFigura() + "]" + " has ganado!!!!!!");
+        } else {
+            System.out.println("EMPATE!!!!!!!!!");
+        }
+    }
+
+    private static void limpiarPantalla() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println();
+        }
+    }
+}
---
+++ trunk/src/Tablero.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class Tablero{
+    private String[] tab = new String[9];
+    private String ganador = null;
+
+    public Tablero(){
+        for(int i=0 ; i<tab.length ; i++){
+            tab[i] = Integer.toString(i+1);
+        }
+    }
+
+    public void imprimirTablero(){
+        int cont = 0;
+        System.out.print("\t\t\t");
+        for(int i=0 ; i<tab.length ; i++){
+            System.out.print("  "+tab[i]+"  ");
+            cont++;
+            if(cont == 3){
+                System.out.print("\n\n");
+                System.out.print("\t\t\t");
+                cont = 0;
+            }
+        }
+    }
+
+    public void setFigura(String fig, int num){
+        tab[num-1] = fig;
+    }
+
+    public boolean tableroLleno(){
+        int cont = 0;
+        for(int i=0 ; i<tab.length ; i++){
+            if(!tab[i].equalsIgnoreCase(Integer.toString(i+1))){
+                cont++;
+            }
+        }
+        if(cont == 9)
+            return true;
+        else return false;
+    }
+
+    public boolean posicionOcupada(int posicion){
+        if(tab[posicion-1].equalsIgnoreCase("x")
+        || tab[posicion-1].equalsIgnoreCase("o"))
+            return true;
+        else
+            return false;
+    }
+
+    public boolean hayGanador(){
+        if(tab[0].equalsIgnoreCase(tab[1]) && tab[1].equalsIgnoreCase(tab[2])){
+            ganador = tab[0];
+            return true;
+        }else if(tab[3].equalsIgnoreCase(tab[4]) && tab[4].equalsIgnoreCase(tab[5])){
+            ganador = tab[3];
+            return true;
+        }else if(tab[6].equalsIgnoreCase(tab[7]) && tab[7].equalsIgnoreCase(tab[8])){
+            ganador = tab[6];
+            return true;
+        }else if(tab[0].equalsIgnoreCase(tab[3]) && tab[3].equalsIgnoreCase(tab[6])){
+            ganador = tab[0];
+            return true;
+        }else if(tab[1].equalsIgnoreCase(tab[4]) && tab[4].equalsIgnoreCase(tab[7])){
+            ganador = tab[1];
+            return true;
+        }else if(tab[2].equalsIgnoreCase(tab[5]) && tab[5].equalsIgnoreCase(tab[8])){
+            ganador = tab[2];
+            return true;
+        }else if(tab[0].equalsIgnoreCase(tab[4]) && tab[4].equalsIgnoreCase(tab[8])){
+            ganador = tab[0];
+            return true;
+        }else if(tab[2].equalsIgnoreCase(tab[4]) && tab[4].equalsIgnoreCase(tab[6])){
+            ganador = tab[2];
+            return true;
+        }else return false;
+    }
+
+    public String getGanador(){
+        return ganador;
+    }
+}
